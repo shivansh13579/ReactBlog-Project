456b4dd05b681abd4e3cd3c4267783ff0468587b
shivansh13579 <shivansh0975@gmail.com>
2023-10-04 21:51:57 +0530

    create authService

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className='bg-slate-900 text-white p-4'>BlogProject</h1>
+       console.log(import.meta.env.VITE_APPWRITE_URL)
+    </>
+  )
+}
+
+export default App
