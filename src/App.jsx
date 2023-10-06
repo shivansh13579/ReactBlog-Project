c1b9ccc248208e35a276203d6a199b7a8409f57a
shivansh13579 <shivansh0975@gmail.com>
2023-10-06 21:55:05 +0530

    create header and footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,38 @@
-import { useState } from 'react'
+import { useState,useEffect } from 'react'
 import './App.css'
+import { useDispatch } from 'react-redux'
+import authService from './appwrite/auth'
+import { login,logout } from './store/authSlice'
+import { Footer, Header } from './components'
+import { Outlet } from 'react-router-dom'
 
 function App() {
+  const [loading,setLoading] = useState(true)
+  const dispatch = useDispatch()
 
-  return (
-    <>
-      <h1 className='bg-slate-900 text-white p-4'>BlogProject</h1>
-    </>
-  )
+  useEffect(()=>{
+         authService.getCurrentUser()
+         .then((userData)=>{
+          if(userData){
+            dispatch(login({userData}))
+          }else{
+            dispatch(logout())
+          }
+         })
+         .finally(()=>setLoading(false))
+  },[])
+
+         return !loading ? (
+          <div className='min-h-screen flex flex-wrap content-between bg-gray-400'>
+                <div className='w-full block'>
+                    <Header/>
+                    <main>
+                    Todo :  {/* <Outlet/> */}
+                    </main>
+                    <Footer/>
+                </div>
+          </div>
+         ) : null
 }
 
 export default App
