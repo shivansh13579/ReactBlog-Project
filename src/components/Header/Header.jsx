870439fd5b8a6d4ccbe56f36720fcf36baa106de
shivansh13579 <shivansh0975@gmail.com>
2023-10-08 09:38:12 +0530

    add button and input component

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -48,8 +48,20 @@ function Header() {
             <ul className='flex ml-auto'>
               {navItems.map((item)=>
               item.active ? (
-                <li></li>
+                <li key={item.name}>
+                  <button
+                  onClick={()=>navigate(item.slug)} 
+                  className='inline-block px-6 py-2 duration-200 hover:bg-blue-100 rounded-full'
+                   >
+                  {item.name}
+                  </button>
+                </li>
               ) : null
+              )}
+              {authStatus && (
+                <li>
+                  <LogoutBtn/>
+                </li>
               )}
             </ul>
           </nav>
@@ -59,3 +71,62 @@ function Header() {
 }
 
 export default Header
+
+// import React from 'react'
+// import { Container,Logo,LogoutBtn } from "../index"
+// import { useSelector } from 'react-redux'
+// import { useNavigate } from 'react-router-dom'
+// import { Link } from 'react-router-dom'
+
+// function Header() {
+//   const navigate = useNavigate()
+//   const authStatus = useSelector((state)=> state.auth.status)
+
+//   const navItem = [
+//     {
+//       name : "Home",
+//       slug : '/',
+//       active: true
+//     },
+//     {
+//       name : "Login",
+//       slug : '/login',
+//       active: !authStatus
+//     },
+//     {
+//       name : "Signup",
+//       slug : '/signup',
+//       active: !authStatus
+//     },
+//     {
+//       name : "All Post",
+//       slug : '/all-post',
+//       active: authStatus
+//     },
+//     {
+//       name : "Add Post",
+//       slug : '/add-post',
+//       active: authStatus
+//     },
+//   ]
+
+//   return (
+//       <header>
+//        <Container>
+//         <nav>
+//           <div>
+//           <Link to='/'>
+//             <Logo  width='70px'/>
+//             </Link>
+//           </div>
+//           <ul>
+//             {navItem.map((item)=>)}
+//           </ul>
+//         </nav>
+//        </Container>
+//       </header>
+//   )
+// }
+
+// export default Header
+
--- src/components/Header/LogoutBtn.jsx
+++ src/components/Header/LogoutBtn.jsx
@@ -13,11 +13,13 @@ function LogoutBtn(){
   return (
       <button 
       className='inline-bock px-6 py-2 duration-200 hover:bg-blue-100 rounded-full'
-      onClick={logoutHandler()}
+      onClick={logoutHandler}
       >
       Logout
       </button>
   )
 }
 
-export default LogoutBtn
+// export default LogoutBtn
+
+
