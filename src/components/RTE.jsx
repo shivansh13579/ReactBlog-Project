6c16a19d9900c2a28ddd6c12f044a7f02aab2680
shivansh13579 <shivansh0975@gmail.com>
2023-10-11 21:55:41 +0530

    create post form

--- src/components/RTE.jsx
+++ src/components/RTE.jsx
@@ -8,7 +8,7 @@ export default function RTE({name,control,label,defaultValue = ""}) {
       {label && <label className='inline-block mb-1 pl-1'>{label}</label>}
 
       <Controller
-        name={name}
+        name={name || "content"}
         control={control}
         render={({field: {onChange}})=>(
            <Editor
@@ -17,8 +17,30 @@ export default function RTE({name,control,label,defaultValue = ""}) {
                 initialValue: defaultValue,
                 height:500,
                 menubar: true,
-                plugins:""
-            }}
+                plugins:[ "image",
+                "advlist",
+                "autolink",
+                "lists",
+                "link",
+                "image",
+                "charmap",
+                "preview",
+                "anchor",
+                "searchreplace",
+                "visualblocks",
+                "code",
+                "fullscreen",
+                "insertdatetime",
+                "media",
+                "table",
+                "code",
+                "help",
+                "wordcount",
+                "anchor",],
+              toolbar:"undo redo | blocks | image | bold italic forecolor | alignleft aligncenter bold italic forecolor | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent |removeformat | help",
+              content_style: "body { font-family:Helvetica,Arial,sans-serif; font-size:14px }"
+        }}
+        onEditorChange={onChange}
            />
         )}
       />
--- src/components/index.js
+++ src/components/index.js
@@ -3,6 +3,8 @@ import Footer from "./Footer/Footer";
 import Container from "./container/Container";
 import Logo from "./Logo";
 import LogoutBtn from "./Header/LogoutBtn";
+import Select from "./Select";
+import RTE from "./RTE";
 
 export {
     Header,
@@ -10,4 +12,7 @@ export {
     Container,
     Logo,
     LogoutBtn,
+    Select,
+    RTE,
+    
 }
